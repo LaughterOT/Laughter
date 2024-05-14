3af78c211b08c32068881abcdb34ed78efdc90d0
LaughterOT <169160374+LaughterOT@users.noreply.github.com>
2024-05-14 14:14:28 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,53 @@
+
+//---- TASK ONE ----
+
+let a = 3;
+let b = 5;
+let c ;
+
+let result = `
+a + b = ${a + b};
+
+a - b = ${a - b};
+
+a * b = ${a * b};
+
+a / b = ${a / b};
+
+a % b = ${a % b};
+
+a += b = ${a += b};
+
+a -= b = ${a -= b};
+
+a *= b = ${a *= b};
+
+a /= b = ${a /= b};
+
+a %= b = ${a %= b};
+
+a == b = ${a == b};
+
+a != b = ${a != b};
+
+a > b = ${a > b};
+
+a < b = ${a < b};
+
+!a && !c = ${!a && !c};
+
+!a || !c = ${!a || !c};
+
+`
+console.log(result);
+
+// ---- TASK TWO ---- 
+
+let first_name = "Laughter";
+let last_name = "Otulana";
+let email = "otul0001@algonquinlive.com";
+
+console.log ( `My name is ${first_name} ${last_name}. You can contact me at ${email}.`);
+
+
+
